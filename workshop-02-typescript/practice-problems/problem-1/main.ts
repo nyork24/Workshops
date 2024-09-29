519d7bd12fcb01f1de990c90d5143ef0c84fe0f7
nyork24 <nolanblox@gmail.com>
2024-09-29 12:55:51 -0400

    ts problem 1

--- workshop-02-typescript/practice-problems/problem-1/main.ts
+++ workshop-02-typescript/practice-problems/problem-1/main.ts
@@ -1,1 +1,14 @@
 // Fill in for exercises!
+
+let userName = 'Nolan'
+
+const favoriteFoods = ['pizza', 'mac and cheese', 'ice cream']
+
+const user = {
+    name: userName,
+    age: 3,
+    favoriteFoods: favoriteFoods
+}
+
+console.log(user)
+console.log(user.favoriteFoods[0])
